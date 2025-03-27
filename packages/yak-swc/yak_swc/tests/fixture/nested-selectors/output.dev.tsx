b4890e00a5a7163a5105b881f22f81d16dd7d025
Christian Klauser <christian.klauser@digitecgalaxus.ch>
2025-03-27 23:55:26 +0100

    Update snapshots for dev mode (displayNames: true)

--- packages/yak-swc/yak_swc/tests/fixture/nested-selectors/output.dev.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/nested-selectors/output.dev.tsx
@@ -30,4 +30,6 @@ export const Card = /*YAK Extracted CSS:
     }
   }
 }
-*/ /*#__PURE__*/ __yak.__yak_div(__styleYak.Card);
+*/ /*#__PURE__*/ globalThis.Object.assign(/*#__PURE__*/ __yak.__yak_div(__styleYak.Card), {
+    "displayName": "Card"
+});
