c12dbba2797df4a6c2649a043bab6efcebb092e4
Jan Nicklas <j.nicklas@me.com>
2025-05-07 16:12:17 +0200

    Improve linting rule type definition

---
+++ .changeset/five-cows-sort.md
@@ -0,0 +1,5 @@
+---
+"eslint-plugin-yak": patch
+---
+
+Improve linting rule type definition
--- packages/eslint-plugin-yak/index.ts
+++ packages/eslint-plugin-yak/index.ts
@@ -1,17 +1,18 @@
+import type { ESLint, Linter } from "eslint";
 import pkg from "./package.json" with { type: "json" };
 import { cssNestingOperator } from "./rules/cssNestingOperator.js";
 import { enforceSemicolons } from "./rules/enforceSemicolon.js";
 import { styleConditions } from "./rules/styleConditions.js";
 
-const plugin = {
+const plugin: ESLint.Plugin = {
   meta: {
     name: pkg.name,
     version: pkg.version,
   },
   rules: {
-    "css-nesting-operator": cssNestingOperator,
-    "enforce-semicolon": enforceSemicolons,
-    "style-conditions": styleConditions,
+    "css-nesting-operator": cssNestingOperator as any,
+    "enforce-semicolon": enforceSemicolons as any,
+    "style-conditions": styleConditions as any,
   },
   processors: {},
 };
@@ -27,7 +28,7 @@ const configs = {
       [`${pkg.name}/style-conditions`]: "warn",
     },
   },
-};
+} as const satisfies { recommended: Linter.Config };
 
 export default Object.assign(plugin, {
   configs,
