a2de4da77ed6ae803390a0f89a0a2ec0b9447764
Luca Schneider <luca.schneider@gmx.ch>
2025-08-31 11:27:10 +0200

    Enable export default expressions

--- packages/yak-swc/yak_swc/tests/fixture/default-export-inline/output.prod.tsx
+++ packages/yak-swc/yak_swc/tests/fixture/default-export-inline/output.prod.tsx
@@ -1,7 +1,7 @@
 import { styled } from "next-yak/internal";
 import * as __yak from "next-yak/internal";
 import "./input.yak.module.css!=!./input?./input.yak.module.css";
-export default /*YAK Extracted CSS:
+export default /*YAK EXPORTED STYLED:default:ym7uBBu*//*YAK Extracted CSS:
 :global(.ym7uBBu) {
   color: blue;
   font-size: 24px;
