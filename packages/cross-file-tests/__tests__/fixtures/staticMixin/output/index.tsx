38d84e66b25d70ebfb2be8213b7a32c4fbf553d0
Luca Schneider <luca.schneider@gmx.ch>
2025-10-27 09:59:33 +0100

    Update snapshots

--- packages/cross-file-tests/__tests__/fixtures/staticMixin/output/index.tsx
+++ packages/cross-file-tests/__tests__/fixtures/staticMixin/output/index.tsx
@@ -2,7 +2,7 @@ import { styled } from "next-yak/internal";
 import { lastChildMixin } from './mixin';
 import * as __yak from "next-yak/internal";
 import "./index.yak.module.css!=!./index?./index.yak.module.css";
-var ListItem = /*YAK Extracted CSS:
+var ListItem = /*YAK EXPORTED STYLED:ListItem:index_ListItem_PZqRtI*//*YAK Extracted CSS:
 :global(.index_ListItem_PZqRtI) {
   margin-bottom: 10px;
   &:hover {
@@ -12,4 +12,11 @@ var ListItem = /*YAK Extracted CSS:
 */ /*#__PURE__*/ Object.assign(/*#__PURE__*/ __yak.__yak_li("index_ListItem_PZqRtI"), {
     "displayName": "ListItem"
 });
-export default ListItem;
+export default /*YAK EXPORTED STYLED:default:index_ListItem_PZqRtI*//*YAK Extracted CSS:
+:global(.index_ListItem_PZqRtI) {
+  margin-bottom: 10px;
+  &:hover {
+    --yak-css-import: url("./mixin:lastChildMixin",mixin);
+  }
+}
+*/ ListItem;
