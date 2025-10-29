2cd5cae4165a3a425d391748d7d9efcfbac747af
Luca Schneider <luca.schneider@gmx.ch>
2025-10-29 11:22:49 +0100

    Fix tests

---
+++ packages/yak-swc/yak_swc/tests/fixture/default-export-with-cross-file-reference/output.turbo.dev.tsx
@@ -0,0 +1,26 @@
+import { styled } from "next-yak/internal";
+import Text from "./text";
+import * as __yak from "next-yak/internal";
+import "data:text/css;base64,LmlucHV0X0JveF9tN3VCQnUgewogIHBhZGRpbmc6IDMycHg7CiAgYmFja2dyb3VuZDogIzMzMzsKICAtLXlhay1jc3MtaW1wb3J0OiB1cmwoIi4vdGV4dDpkZWZhdWx0IixzZWxlY3RvcikgewogICAgY29sb3I6ICMwMGZmMDA7CiAgICBmb250LXdlaWdodDogYm9sZDsKICB9Cn0=";
+const Box = /*YAK EXPORTED STYLED:Box:input_Box_m7uBBu*//*YAK Extracted CSS:
+.input_Box_m7uBBu {
+  padding: 32px;
+  background: #333;
+  --yak-css-import: url("./text:default",selector) {
+    color: #00ff00;
+    font-weight: bold;
+  }
+}
+*/ /*#__PURE__*/ Object.assign(/*#__PURE__*/ __yak.__yak_div("input_Box_m7uBBu"), {
+    "displayName": "Box"
+});
+export default /*YAK EXPORTED STYLED:default:input_Box_m7uBBu*//*YAK Extracted CSS:
+.input_Box_m7uBBu {
+  padding: 32px;
+  background: #333;
+  --yak-css-import: url("./text:default",selector) {
+    color: #00ff00;
+    font-weight: bold;
+  }
+}
+*/ Box;
