632ea9fd64c3425591f57c53e83e7ad0cb7fa8e6
Jan Nicklas <j.nicklas@me.com>
2026-03-18 14:37:16 +0100

    Add e2e test for CSS url() asset references
    Reproduces #513 — verifies that assets referenced via CSS url()
    in styled components are served correctly by the bundler.

---
+++ e2e/cases/css-url-asset/App.tsx
@@ -0,0 +1,5 @@
+import { ImageBox } from "./img/Img";
+
+export default function App() {
+  return <ImageBox data-testid="image-box" />;
+}
---
+++ e2e/cases/css-url-asset/img/Img.tsx
@@ -0,0 +1,7 @@
+import { styled } from "next-yak";
+
+export const ImageBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background: url("./assets/yak-jumping.png") no-repeat center / contain;
+`;
