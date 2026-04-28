c4bdec9fa16bdf060856171c571c159de8d69c67
Luca Schneider <luca.schneider@gmx.ch>
2026-04-28 13:29:44 +0200

    Report error on at rule interpolation

---
+++ packages/yak-swc/yak_swc/tests/fixture/dynamic-at-rule-error/output.prod.tsx
@@ -0,0 +1,13 @@
+import { styled } from "next-yak/internal";
+import * as __yak from "next-yak/internal";
+import "./input.yak.module.css!=!./input?./input.yak.module.css";
+// Dynamic interpolation inside @media query is not valid CSS:
+// the browser cannot read CSS variables before the media query is evaluated.
+const Box = /*YAK Extracted CSS:
+:global(.ym7uBBu) {
+  background: red;
+  @media {
+    display: none;
+  }
+}
+*/ /*#__PURE__*/ __yak.__yak_div("ym7uBBu", (p)=>p.theme.queries.desktopAndAbove);
